513c15462c1dd9a8740ce04534e5a873368ddd5c
luosmnazh <shinveronika06@gmail.com>
2024-10-31 11:09:12 +0500

    Added some models.
    Created User and DriverLicense models.
    Added 'users' app to installed apps in settings.
    Created blank controllers in users.views.
    Added migrations.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+class User(AbstractUser):
+    is_driver = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username
+
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        if self.is_driver:
+            self.driver_license = None
+
+
+class DriverLicense(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='driver_license', primary_key=True)
+
+    surname = models.CharField(max_length=50)
+    name_patronymic = models.CharField(max_length=100)
+    iin = models.CharField(max_length=12, unique=True, db_index=True)
+    date_of_issue = models.DateField()
+    date_of_expiry = models.DateField()
+    license_number = models.CharField(max_length=10, unique=True, db_index=True)
+    photo = models.ImageField(upload_to='driver_license_photos', null=True, blank=True)
+    category = models.ManyToManyField('DriverLicenseCategory')
+
+    def __str__(self):
+        return f'{self.surname} {self.name_patronymic} {self.license_number}'
+
+
+class DriverLicenseCategory(models.Model):
+    name = models.CharField(max_length=5, unique=True, db_index=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def user_login(request):
+    return render(request, 'users/login.html')
+
+
+def user_logout(request):
+    return render(request, 'users/logout.html')
+
+
+def user_register(request):
+    return render(request, 'users/register.html')
